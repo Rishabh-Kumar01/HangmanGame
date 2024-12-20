d7c96c89f2e9fddeb6d5e6993a1d08feb151a11f
Rishabh-Kumar01 <rishabh1teotia@gmail.com>
2024-12-20 23:35:45 +0530

    refactor: Remove StrictMode wrapper from main rendering and clean up StartGame component formatting

--- src/pages/StartGame.jsx
+++ src/pages/StartGame.jsx
@@ -1,15 +1,15 @@
-import React from 'react'
-import TextInputFormContainer from '../components/TextInputForm/TextInputFormContainer'
-import { Link } from 'react-router-dom'
+import React from "react";
+import TextInputFormContainer from "../components/TextInputForm/TextInputFormContainer";
+import { Link } from "react-router-dom";
 
 function StartGame() {
   return (
     <>
-        <h1>Start Game</h1>
-        <TextInputFormContainer />
-        <Link to="/game">Play Game</Link>
+      <h1>Start Game</h1>
+      <TextInputFormContainer />
+      <Link to="/game">Play Game</Link>
     </>
-  )
+  );
 }
 
-export default StartGame
+export default StartGame;
